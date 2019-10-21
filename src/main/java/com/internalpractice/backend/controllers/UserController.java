f9a11d6b0c3eec288fed399febd1253be4e64312
Alan Jimenez Arandia <aljiar23@gmail.com>
2019-10-21 15:35:29 -0400

    [#4] Implement User entity, service, repository and controller.

---
+++ src/main/java/com/internalpractice/backend/controllers/UserController.java
@@ -0,0 +1,48 @@
+package com.internalpractice.backend.controllers;
+
+import com.internalpractice.backend.entities.User;
+import com.internalpractice.backend.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Inject
+    private UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createUser(@RequestBody User user){
+        userService.storeUser(user);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public List<User> getUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{userId}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public User getUser(@PathVariable Long userId){
+        return userService.getUserById(userId);
+    }
+
+    @DeleteMapping("/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long userId){
+        userService.deleteUser(userId);
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void updateUser(@PathVariable User user){
+        userService.storeUser(user);
+    }
+
+
+}
---
+++ src/main/java/com/internalpractice/backend/entities/User.java
@@ -0,0 +1,134 @@
+package com.internalpractice.backend.entities;
+
+import javax.persistence.Entity;
+
+    @Entity
+    public class User {
+        private Long id;
+        private String userId;
+        private String firstName;
+        private String secondName;
+        private String firstLastName;
+        private String secondLastName;
+        private String email;
+        private String personalEmail;
+        private int phone;
+        private int cellphone;
+        private String workArea;
+        private String position;
+
+        public User(){
+
+        }
+
+        public User(String userId, String firstName, String secondName, String firstLastName, String secondLastName,
+                    String email, String personalEmail, int phone, int cellphone, String workArea, String position) {
+            this.userId = userId;
+            this.firstName = firstName;
+            this.secondName = secondName;
+            this.firstLastName = firstLastName;
+            this.secondLastName = secondLastName;
+            this.email = email;
+            this.personalEmail = personalEmail;
+            this.phone = phone;
+            this.cellphone = cellphone;
+            this.workArea = workArea;
+            this.position = position;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getUserId() {
+            return userId;
+        }
+
+        public void setUserId(String userId) {
+            this.userId = userId;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getSecondName() {
+            return secondName;
+        }
+
+        public void setSecondName(String secondName) {
+            this.secondName = secondName;
+        }
+
+        public String getFirstLastName() {
+            return firstLastName;
+        }
+
+        public void setFirstLastName(String firstLastName) {
+            this.firstLastName = firstLastName;
+        }
+
+        public String getSecondLastName() {
+            return secondLastName;
+        }
+
+        public void setSecondLastName(String secondLastName) {
+            this.secondLastName = secondLastName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPersonalEmail() {
+            return personalEmail;
+        }
+
+        public void setPersonalEmail(String personalEmail) {
+            this.personalEmail = personalEmail;
+        }
+
+        public int getPhone() {
+            return phone;
+        }
+
+        public void setPhone(int phone) {
+            this.phone = phone;
+        }
+
+        public int getCellphone() {
+            return cellphone;
+        }
+
+        public void setCellphone(int cellphone) {
+            this.cellphone = cellphone;
+        }
+
+        public String getWorkArea() {
+            return workArea;
+        }
+
+        public void setWorkArea(String workArea) {
+            this.workArea = workArea;
+        }
+
+        public String getPosition() {
+            return position;
+        }
+
+        public void setPosition(String position) {
+            this.position = position;
+        }
+    }
